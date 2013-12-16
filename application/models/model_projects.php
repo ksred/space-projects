65819faffdceea34d73f3281a11615b212375e3f
Kyle Redelinghuys <kyle@ksred.me>
2013-12-16 07:17:20 +0200

    Removed redundant code

--- application/models/model_projects.php
+++ application/models/model_projects.php
@@ -33,38 +33,5 @@ class Model_projects extends CI_Model
 		return $result;
     }
 
-    function update_task ($id, $status) {
-        $this->db->where("id", $id);
-        $result = $this->db->update("tasks", array("status" => $status));
-        return $result;
-    }
-
-    function update_order_item_meta($user_id, $order_id, $item_meta_id) {
-        $this->db->where("user_id", $user_id);
-        $this->db->where("id", $order_id);
-        $data = array("item_meta_id", $item_meta_id);
-        $result = $this->db->update("orders_items", $data);
-        return $result;
-    }
-
-    function delete_order($user_id, $order_id) {
-        $data = array("user_id" => $user_id, "id" => $order_id);
-        $result_o = $this->db->delete("orders", $data);
-        $data = array("user_id" => $user_id, "order_id" => $order_id);
-        $result_oi = $this->db->delete("orders_items", $data);
-        $result_on = $this->db->delete("orders_notes", $data);
-        //Make sure all were successful
-        if (($result_o == 1) && ($result_oi == 1) && ($result_on == 1)) {
-        	return 1;
-        } else {
-        	return 0;
-        }
-    }
-
-    function delete_order_item($user_id, $order_id, $item_id, $order_item_id) {
-        $data = array("user_id" => $user_id, "order_id" => $order_id, "item_id" => $item_id, "id" => $order_item_id);
-        $result = $this->db->delete("orders_items", $data);
-		return $result;
-    }
 }
 ?>
